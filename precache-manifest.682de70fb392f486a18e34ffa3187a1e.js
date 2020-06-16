447dab692e6a7eb31c9c23644ca03af604be8da5
ujjwal <ujjwal24batra@gmail.com>
2020-06-16 22:50:19 +0530

    Updates

---
+++ precache-manifest.682de70fb392f486a18e34ffa3187a1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480818274adb7d56c65295b49b564ac9",
+    "url": "/Danube-Web/index.html"
+  },
+  {
+    "revision": "cd86b2400198fa4f2f38",
+    "url": "/Danube-Web/static/css/main.24e39de4.chunk.css"
+  },
+  {
+    "revision": "aa50626d71202d7e6331",
+    "url": "/Danube-Web/static/js/2.356ddda8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Danube-Web/static/js/2.356ddda8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd86b2400198fa4f2f38",
+    "url": "/Danube-Web/static/js/main.54c4d29d.chunk.js"
+  },
+  {
+    "revision": "d41dad5560e82a401fe1",
+    "url": "/Danube-Web/static/js/runtime-main.b171ac7e.js"
+  }
+]);
